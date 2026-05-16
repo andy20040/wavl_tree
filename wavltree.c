e5820438eb8ac539fc9f83e009a71f24293c108e
F74111128 <andy930807@gmail.com>
2026-05-16 20:15:25 +0800

    finish single rotation ,not yet finish double rotation

--- wavltree.c
+++ wavltree.c
@@ -74,47 +74,34 @@ EXPORT_SYMBOL(wavl_link_node);
 
 
 void wavl_insert_color(struct rb_node *node, struct rb_root *root) {
-    struct rb_node *parent = rb_red_parent(node), *gparent, *tmp;
-    if (unlikely(!parent)) {
+    struct rb_node *parent = rb_red_parent(node), *gparent, *sibling;
+    while(true){
+        if (unlikely(!parent)) {
 			break;
 		}
-
-    // 新插入的節點預設與 parent 的 rank 差值為 0 (產生 0-1 違規)
-    wavl_set_rank_diff(node, 0); 
-
-    while (node != root->rb_node) {
-    struct rb_node *parent = rb_parent(node);
-    
-    // 如果跟爸爸的差值是 1 或 2，代表已經平衡，直接結束
-    if (wavl_rank_diff(parent, node) > 0)
-        break; 
-
-    // 以下是差值為 0 的處理 (0-child violation)
-    struct rb_node *sibling = wavl_sibling(node);
-
-    // 【第一招：Promote】 (對應圖片第一排)
-    if (wavl_rank_diff(parent, sibling) == 1) {
-        wavl_promote(parent); // 爸爸 Rank + 1 (如果是存 Parity 就 XOR 1)
-        node = parent;        // 違規可能上傳，往上爬繼續 while
-        continue;
-    }
-
-    // 【準備旋轉】(因為 sibling diff == 2)
-    // 判斷是 LL 型還是 LR 型，決定用單旋還是雙旋
-    if ( node 是 parent 的左小孩 ) {
-        if ( node 的左小孩 rank diff == 1 ) {
-            // 【第二招：單旋轉】
-            // ... 執行右旋，調 Rank ...
-        } else {
-            // 【第三招：雙旋轉】
-            // ... 執行左旋再右旋，調 Rank ...
+        if (wavl_rank_diff(parent, node) != 0) //check 0-violation
+        break;
+        gparent=wavl_parent(parent);
+        if(node==parent->rb_left){ //node on left
+            sibling=parent->rb_right;
+            if(wavl_rank_diff(sibling)==1){
+                // 0,1 case go promote
+                wavl_flip_parity(parent);
+                node=parent;
+                parent=gparent;
+                continue;
+            }
         }
-    } else {
-        // ... 對稱的右半邊邏輯 ...
-    }
-    
-    // 旋轉完必定完全平衡，直接跳出迴圈！
-    break; 
+        else{   //node on right
+            sibling=parent->rb_left;
+            if(wavl_rank_diff(sibling)==1){
+                // 0,1 case go promote
+                wavl_flip_parity(parent);
+                node=parent;
+                parent=gparent;
+                continue;
+            }
+        }
     }
 }
 EXPORT_SYMBOL(wavl_insert_color);
