91648aedafebe406f15f92fcf0048e4b1634a5ff
F74111128 <andy930807@gmail.com>
2026-06-04 21:59:49 +0800

    add delete test

--- rbtree_wrapper_mod.c
+++ rbtree_wrapper_mod.c
@@ -11,7 +11,6 @@
 #define PROC_NAME "rbtree_test_cmd"
 DEFINE_PER_CPU(u64, baseline_rotations);
 DEFINE_PER_CPU(u64, baseline_path_length);
-
 struct rb_root my_test_tree = RB_ROOT; //rbtree
 struct rb_root my_wavl_tree = RB_ROOT; //wavl_tree
 
@@ -177,12 +176,229 @@ static noinline void my_rb_insert_wrapper(struct rb_node *node, struct rb_root *
         }
 }
 
+static __always_inline void
+____myrb_erase_color(struct rb_node *parent, struct rb_root *root,
+	void (*augment_rotate)(struct rb_node *old, struct rb_node *new))
+{
+	struct rb_node *node = NULL, *sibling, *tmp1, *tmp2;
+
+	while (true) {
+		/*
+		 * Loop invariants:
+		 * - node is black (or NULL on first iteration)
+		 * - node is not the root (parent is not NULL)
+		 * - All leaf paths going through parent and node have a
+		 *   black node count that is 1 lower than other leaf paths.
+		 */
+        this_cpu_inc(baseline_path_length);
+		sibling = parent->rb_right;
+		if (node != sibling) {	/* node == parent->rb_left */
+			if (rb_is_red(sibling)) {
+				/*
+				 * Case 1 - left rotate at parent
+				 *
+				 *     P               S
+				 *    / \             / \
+				 *   N   s    -->    p   Sr
+				 *      / \         / \
+				 *     Sl  Sr      N   Sl
+				 */
+				tmp1 = sibling->rb_left;
+				WRITE_ONCE(parent->rb_right, tmp1);
+				WRITE_ONCE(sibling->rb_left, parent);
+				rb_set_parent_color(tmp1, parent, RB_BLACK);
+				__rb_rotate_set_parents(parent, sibling, root,
+							RB_RED);
+				augment_rotate(parent, sibling);
+                this_cpu_inc(baseline_rotations);
+				sibling = tmp1;
+			}
+			tmp1 = sibling->rb_right;
+			if (!tmp1 || rb_is_black(tmp1)) {
+				tmp2 = sibling->rb_left;
+				if (!tmp2 || rb_is_black(tmp2)) {
+					/*
+					 * Case 2 - sibling color flip
+					 * (p could be either color here)
+					 *
+					 *    (p)           (p)
+					 *    / \           / \
+					 *   N   S    -->  N   s
+					 *      / \           / \
+					 *     Sl  Sr        Sl  Sr
+					 *
+					 * This leaves us violating 5) which
+					 * can be fixed by flipping p to black
+					 * if it was red, or by recursing at p.
+					 * p is red when coming from Case 1.
+					 */
+					rb_set_parent_color(sibling, parent,
+							    RB_RED);
+					if (rb_is_red(parent))
+						rb_set_black(parent);
+					else {
+						node = parent;
+						parent = rb_parent(node);
+						if (parent)
+							continue;
+					}
+					break;
+				}
+				/*
+				 * Case 3 - right rotate at sibling
+				 * (p could be either color here)
+				 *
+				 *   (p)           (p)
+				 *   / \           / \
+				 *  N   S    -->  N   sl
+				 *     / \             \
+				 *    sl  Sr            S
+				 *                       \
+				 *                        Sr
+				 *
+				 * Note: p might be red, and then both
+				 * p and sl are red after rotation(which
+				 * breaks property 4). This is fixed in
+				 * Case 4 (in __rb_rotate_set_parents()
+				 *         which set sl the color of p
+				 *         and set p RB_BLACK)
+				 *
+				 *   (p)            (sl)
+				 *   / \            /  \
+				 *  N   sl   -->   P    S
+				 *       \        /      \
+				 *        S      N        Sr
+				 *         \
+				 *          Sr
+				 */
+				tmp1 = tmp2->rb_right;
+				WRITE_ONCE(sibling->rb_left, tmp1);
+				WRITE_ONCE(tmp2->rb_right, sibling);
+				WRITE_ONCE(parent->rb_right, tmp2);
+				if (tmp1)
+					rb_set_parent_color(tmp1, sibling,
+							    RB_BLACK);
+				augment_rotate(sibling, tmp2);
+                this_cpu_inc(baseline_rotations);
+				tmp1 = sibling;
+				sibling = tmp2;
+			}
+			/*
+			 * Case 4 - left rotate at parent + color flips
+			 * (p and sl could be either color here.
+			 *  After rotation, p becomes black, s acquires
+			 *  p's color, and sl keeps its color)
+			 *
+			 *      (p)             (s)
+			 *      / \             / \
+			 *     N   S     -->   P   Sr
+			 *        / \         / \
+			 *      (sl) sr      N  (sl)
+			 */
+			tmp2 = sibling->rb_left;
+			WRITE_ONCE(parent->rb_right, tmp2);
+			WRITE_ONCE(sibling->rb_left, parent);
+			rb_set_parent_color(tmp1, sibling, RB_BLACK);
+			if (tmp2)
+				rb_set_parent(tmp2, parent);
+			__rb_rotate_set_parents(parent, sibling, root,
+						RB_BLACK);
+			augment_rotate(parent, sibling);
+            this_cpu_inc(baseline_rotations);
+			break;
+		} else {
+			sibling = parent->rb_left;
+			if (rb_is_red(sibling)) {
+				/* Case 1 - right rotate at parent */
+				tmp1 = sibling->rb_right;
+				WRITE_ONCE(parent->rb_left, tmp1);
+				WRITE_ONCE(sibling->rb_right, parent);
+				rb_set_parent_color(tmp1, parent, RB_BLACK);
+				__rb_rotate_set_parents(parent, sibling, root,
+							RB_RED);
+				augment_rotate(parent, sibling);
+                this_cpu_inc(baseline_rotations);
+				sibling = tmp1;
+			}
+			tmp1 = sibling->rb_left;
+			if (!tmp1 || rb_is_black(tmp1)) {
+				tmp2 = sibling->rb_right;
+				if (!tmp2 || rb_is_black(tmp2)) {
+					/* Case 2 - sibling color flip */
+					rb_set_parent_color(sibling, parent,
+							    RB_RED);
+					if (rb_is_red(parent))
+						rb_set_black(parent);
+					else {
+						node = parent;
+						parent = rb_parent(node);
+						if (parent)
+							continue;
+					}
+					break;
+				}
+				/* Case 3 - left rotate at sibling */
+				tmp1 = tmp2->rb_left;
+				WRITE_ONCE(sibling->rb_right, tmp1);
+				WRITE_ONCE(tmp2->rb_left, sibling);
+				WRITE_ONCE(parent->rb_left, tmp2);
+				if (tmp1)
+					rb_set_parent_color(tmp1, sibling,
+							    RB_BLACK);
+				augment_rotate(sibling, tmp2);
+                this_cpu_inc(baseline_rotations);
+				tmp1 = sibling;
+				sibling = tmp2;
+			}
+			/* Case 4 - right rotate at parent + color flips */
+			tmp2 = sibling->rb_right;
+			WRITE_ONCE(parent->rb_left, tmp2);
+			WRITE_ONCE(sibling->rb_right, parent);
+			rb_set_parent_color(tmp1, sibling, RB_BLACK);
+			if (tmp2)
+				rb_set_parent(tmp2, parent);
+			__rb_rotate_set_parents(parent, sibling, root,
+						RB_BLACK);
+			augment_rotate(parent, sibling);
+            this_cpu_inc(baseline_rotations);
+			break;
+		}
+	}
+}
+
+/* Non-inline version for rb_erase_augmented() use */
+void __myrb_erase_color(struct rb_node *parent, struct rb_root *root,
+	void (*augment_rotate)(struct rb_node *old, struct rb_node *new))
+{
+	____myrb_erase_color(parent, root, augment_rotate);
+}
+EXPORT_SYMBOL(__myrb_erase_color);
+
+
+void my_rb_erase(struct rb_node *node, struct rb_root *root)
+{
+	struct rb_node *rebalance;
+	rebalance = __myrb_erase_augmented(node, root, &dummy_callbacks);
+	if (rebalance)
+		____myrb_erase_color(rebalance, root, dummy_rotate);
+}
+EXPORT_SYMBOL(rb_erase);
+
+
+
+
+
+
+
+
+
+
 
 static void my_rb_insert_color(struct rb_node *node, struct rb_root *root)
 {
 	my_rb_insert_wrapper(node, root, dummy_rotate);
 }
-static void insert_wavl_node(int key)
+static struct my_node* insert_wavl_node(int key)
 {
     struct rb_node **new = &(my_wavl_tree.rb_node), *parent = NULL;
     struct my_node *data = kmalloc(sizeof(struct my_node), GFP_KERNEL);
@@ -198,8 +414,9 @@ static void insert_wavl_node(int key)
     }
     rb_link_node(&data->node, parent, new);
     wavl_insert(&data->node, &my_wavl_tree); 
+    return data;
 }
-static void insert_rb_node(int key) //rbtree
+static struct my_node* insert_rb_node(int key) //rbtree
 {
     struct rb_node **new = &(my_test_tree.rb_node), *parent = NULL;
     struct my_node *data = kmalloc(sizeof(struct my_node), GFP_KERNEL);
@@ -220,15 +437,41 @@ static void insert_rb_node(int key) //rbtree
     rb_link_node(&data->node, parent, new);
     //target
     my_rb_insert_color(&data->node, &my_test_tree);
+    return data;
 }
 
 static ssize_t rbtree_proc_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos)
 {
     int i, cpu;
     u32 random_key;
-    u64 rb_rots = 0, rb_path = 0;
-    u64 wavl_rots = 0, wavl_path = 0;
-    const int TOTAL_OPERATIONS = 1000; 
+    
+    /*  Insert  */
+    u64 rb_ins_rots = 0, rb_ins_path = 0;
+    u64 wavl_ins_rots = 0, wavl_ins_path = 0;
+    
+    /* Delete */
+    u64 rb_del_rots = 0, rb_del_path = 0;
+    u64 wavl_del_rots = 0, wavl_del_path = 0;
+    
+    const int TOTAL_OPERATIONS = 10000;
+    const int DELETE_OPERATIONS = 5000;
+    
+
+    struct my_node **rb_nodes = vmalloc(TOTAL_OPERATIONS * sizeof(struct my_node *));
+    struct my_node **wavl_nodes = vmalloc(TOTAL_OPERATIONS * sizeof(struct my_node *));
+    int *indices = vmalloc(TOTAL_OPERATIONS * sizeof(int)); /* index for shuffle */
+    
+    if (!rb_nodes || !wavl_nodes || !indices) {
+        pr_err("Memory allocation failed!\n");
+        if (rb_nodes) vfree(rb_nodes);
+        if (wavl_nodes) vfree(wavl_nodes);
+        if (indices) vfree(indices);
+        return -ENOMEM;
+    }
+    
+    /* ==========================================
+     * Insert 
+     * ========================================== */
     for_each_possible_cpu(cpu) {
         per_cpu(baseline_rotations, cpu) = 0;
         per_cpu(baseline_path_length, cpu) = 0;
@@ -236,32 +479,105 @@ static ssize_t rbtree_proc_write(struct file *file, const char __user *buf, size
         per_cpu(wavl_path_length, cpu) = 0;
     }
 
-    pr_info("[RB-Test] insert %d nodes...\n", TOTAL_OPERATIONS);
+    pr_info("[RB-Test] Inserting %d nodes...\n", TOTAL_OPERATIONS);
     
     for (i = 0; i < TOTAL_OPERATIONS; i++) {
         random_key = get_random_u32() % 1000000;
-        insert_rb_node(random_key);   
-        insert_wavl_node(random_key);
+        rb_nodes[i]  = insert_rb_node(random_key);   
+        wavl_nodes[i] = insert_wavl_node(random_key);
+        indices[i] = i; 
     }
     
+
     for_each_possible_cpu(cpu) {
-        rb_rots += per_cpu(baseline_rotations, cpu);
-        rb_path += per_cpu(baseline_path_length, cpu);
-        wavl_rots += per_cpu(wavl_rotations, cpu);
-        wavl_path += per_cpu(wavl_path_length, cpu);
+        rb_ins_rots += per_cpu(baseline_rotations, cpu);
+        rb_ins_path += per_cpu(baseline_path_length, cpu);
+        wavl_ins_rots += per_cpu(wavl_rotations, cpu);
+        wavl_ins_path += per_cpu(wavl_path_length, cpu);
     }
 
+    /* ==========================================
+     * shuffle
+     * ========================================== */
+    for (i = TOTAL_OPERATIONS - 1; i > 0; i--) {
+        u32 j = get_random_u32() % (i + 1);
+        
+        int temp = indices[i];
+        indices[i] = indices[j];
+        indices[j] = temp;
+    }
+
+    /* ==========================================
+     * delete
+     * ========================================== */
+    for_each_possible_cpu(cpu) {
+        per_cpu(baseline_rotations, cpu) = 0;
+        per_cpu(baseline_path_length, cpu) = 0;
+        per_cpu(wavl_rotations, cpu) = 0;
+        per_cpu(wavl_path_length, cpu) = 0;
+    }
+
+    pr_info("[RB-Test] Randomly deleting %d nodes...\n", DELETE_OPERATIONS);
+    for (i = 0; i < DELETE_OPERATIONS; i++) {
+        int target_idx = indices[i]; 
+        
+        if (rb_nodes[target_idx]) {
+            my_rb_erase(&rb_nodes[target_idx]->node, &my_test_tree);
+            kfree(rb_nodes[target_idx]);
+            rb_nodes[target_idx] = NULL;
+        }
+        if (wavl_nodes[target_idx]) {
+            wavl_erase(&wavl_nodes[target_idx]->node, &my_wavl_tree);
+            kfree(wavl_nodes[target_idx]);
+            wavl_nodes[target_idx] = NULL;
+        }
+    }
+
+
+    for_each_possible_cpu(cpu) {
+        rb_del_rots += per_cpu(baseline_rotations, cpu);
+        rb_del_path += per_cpu(baseline_path_length, cpu);
+        wavl_del_rots += per_cpu(wavl_rotations, cpu);
+        wavl_del_path += per_cpu(wavl_path_length, cpu);
+    }
+
+
     pr_info("==================================================\n");
-    pr_info("          [ fully insert result graph ]\n");
+    pr_info("               [ Insert Phase ]\n");
     pr_info("==================================================\n");
     pr_info("Metric                     |  RB Tree  | WAVL Tree\n");
     pr_info("--------------------------------------------------\n");
-    pr_info("Total Rotation Counts      | %9llu | %9llu\n", rb_rots, wavl_rots);
+    pr_info("Total Rotation Counts      | %9llu | %9llu\n", rb_ins_rots, wavl_ins_rots);
     pr_info("Average Rotation Counts    | %5llu.%03llu | %5llu.%03llu\n", 
-            rb_rots / TOTAL_OPERATIONS, ((rb_rots * 1000) / TOTAL_OPERATIONS) % 1000,
-            wavl_rots / TOTAL_OPERATIONS, ((wavl_rots * 1000) / TOTAL_OPERATIONS) % 1000);
-    pr_info("Total Rebalancing Path Len | %9llu | %9llu\n", rb_path, wavl_path);
+            rb_ins_rots / TOTAL_OPERATIONS, ((rb_ins_rots * 1000) / TOTAL_OPERATIONS) % 1000,
+            wavl_ins_rots / TOTAL_OPERATIONS, ((wavl_ins_rots * 1000) / TOTAL_OPERATIONS) % 1000);
+    pr_info("Total Rebalancing Path Len | %9llu | %9llu\n", rb_ins_path, wavl_ins_path);
+
     pr_info("==================================================\n");
+    pr_info("               [ Delete Phase ]\n");
+    pr_info("==================================================\n");
+    pr_info("Metric                     |  RB Tree  | WAVL Tree\n");
+    pr_info("--------------------------------------------------\n");
+    pr_info("Total Rotation Counts      | %9llu | %9llu\n", rb_del_rots, wavl_del_rots);
+    pr_info("Average Rotation Counts    | %5llu.%03llu | %5llu.%03llu\n", 
+            rb_del_rots / DELETE_OPERATIONS, ((rb_del_rots * 1000) / DELETE_OPERATIONS) % 1000,
+            wavl_del_rots / DELETE_OPERATIONS, ((wavl_del_rots * 1000) / DELETE_OPERATIONS) % 1000);
+    pr_info("Total Rebalancing Path Len | %9llu | %9llu\n", rb_del_path, wavl_del_path);
+    pr_info("==================================================\n");
+
+
+    vfree(rb_nodes);
+    vfree(wavl_nodes);
+    vfree(indices);
+
+    struct my_node *pos, *n;
+    rbtree_postorder_for_each_entry_safe(pos, n, &my_test_tree, node) {
+        kfree(pos);
+    }
+    rbtree_postorder_for_each_entry_safe(pos, n, &my_wavl_tree, node) {
+        kfree(pos);
+    }
+
     return count;
 }
 
@@ -281,12 +597,7 @@ static void __exit rbtree_wrapper_exit(void)
     struct my_node *pos, *n;
     remove_proc_entry(PROC_NAME, NULL);
     
-    rbtree_postorder_for_each_entry_safe(pos, n, &my_test_tree, node) {
-        kfree(pos);
-    }
-    rbtree_postorder_for_each_entry_safe(pos, n, &my_wavl_tree, node) {
-        kfree(pos);
-    }
+    
     pr_info("[RB-Test] module unloaded \n");
 }
 
