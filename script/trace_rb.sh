8c0e70b4b0719ca14297385e7212a0736f5ac08a
F74111128 <andy930807@gmail.com>
2026-06-19 14:54:52 +0800

    add directory

---
+++ script/trace_rb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+TRACE_DIR=/sys/kernel/tracing  
+
+
+echo 0 > $TRACE_DIR/tracing_on
+echo > $TRACE_DIR/set_ftrace_filter
+echo > $TRACE_DIR/set_ftrace_pid
+
+#set function
+echo my_rb_insert_wrapper > $TRACE_DIR/set_ftrace_filter
+echo my_rb_erase >> $TRACE_DIR/set_ftrace_filter
+
+echo 1 > $TRACE_DIR/function_profile_enabled
+echo "start" > /proc/rbtree_test_cmd
+echo "start ... ( Waiting for  2 seconds )"
+sleep 2
+
+
+echo 0 > $TRACE_DIR/function_profile_enabled
+
+cat $TRACE_DIR/trace_stat/function* | grep my_rb_
